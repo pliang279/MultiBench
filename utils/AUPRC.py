1e3bcb1f0ad90ea266af7063192c45e7f1471f69
Paul Liang <paul.liangpu@gmail.com>
2021-03-15 21:45:28 -0400

    Update AUPRC.py

--- utils/AUPRC.py
+++ utils/AUPRC.py
@@ -5,7 +5,7 @@ def ptsort(tu):
   return tu[0]
 
 def AUPRC(pts):
-  true_labels=[int(x[1]) for x in pts]
-  predicted_probs=[x[0] for x in pts]
+  true_labels = [int(x[1]) for x in pts]
+  predicted_probs = [x[0] for x in pts]
   return sklearn.metrics.average_precision_score(true_labels, predicted_probs)
 
