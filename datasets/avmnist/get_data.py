5544b0568e3cae8e4bbc1855289d653a1b5a2bb3
Wu Zetian <ztwu_nil@zju.edu.cn>
2021-03-21 22:23:19 -0800

    add IMDB and affect dataset

--- datasets/avmnist/get_data.py
+++ datasets/avmnist/get_data.py
@@ -6,7 +6,7 @@ def get_dataloader(data_dir,batch_size=40,num_workers=8,train_shuffle=True,flatt
     trains=[np.load(data_dir+"/image/train_data.npy"),np.load(data_dir+"/audio/train_data.npy"),np.load(data_dir+"/train_labels.npy")]
     tests=[np.load(data_dir+"/image/test_data.npy"),np.load(data_dir+"/audio/test_data.npy"),np.load(data_dir+"/test_labels.npy")]
     if flatten_audio:
-        trains[2]=train[2].reshape(60000,112*112)
+        trains[2]=trains[2].reshape(60000,112*112)
         tests[2]=tests[2].reshape(10000,112*112)
     trainlist=[[trains[j][i] for j in range(3)] for i in range(60000)]
     testlist=[[tests[j][i] for j in range(3)] for i in range(10000)]
