c54605f2e0f9be6777682e7598449cf2b1cd3898
Wu Zetian <ztwu_nil@zju.edu.cn>
2021-04-07 15:05:29 -0700

    bug fixed

--- datasets/affect/get_data.py
+++ datasets/affect/get_data.py
@@ -3,22 +3,36 @@ import pickle
 
 import numpy as np
 import torch
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, Dataset
 
 
 class Affectdataset(Dataset):
     
-    def __init__(self, data:Dict, flatten_time_series:bool) -> None:
+    def __init__(self, data:Dict, flatten_time_series:bool, aligned:bool=True) -> None:
         self.dataset = data
         self.flatten = flatten_time_series
+        self.aligned = aligned
 
     def __getitem__(self, ind):
-        if self.flatten:
-            return [torch.tensor(self.dataset[i][ind]).flatten() \
-                for i in self.dataset.keys() if i != 'id']
+        vision = torch.tensor(self.dataset['vision'][ind])
+        audio = torch.tensor(self.dataset['audio'][ind])
+        text = torch.tensor(self.dataset['text'][ind])
+        if self.aligned:
+            start = text.nonzero()[0][0]
+            vision = vision[start:].float()
+            audio = audio[start:].float()
+            text = text[start:].float()
         else:
-            return [torch.tensor(self.dataset[i][ind]) \
-                for i in self.dataset.keys() if i != 'id']
+            vision = vision[vision.nonzero()[0][0]:].float()
+            audio = audio[audio.nonzero()[0][0]:].float()
+            text = text[text.nonzero()[0][0]:].float()
+
+        if self.flatten:
+            return [vision.flatten(), audio.flatten(), text.flatten(),\
+                    torch.tensor(self.dataset['labels'][ind]).float().flatten(),]
+        else:
+            return [vision, audio, text, torch.tensor(self.dataset['labels'][ind]).float(),]
 
     def __len__(self):
         return self.dataset['id'].shape[0]
@@ -32,10 +46,31 @@ def get_dataloader(
         alldata = pickle.load(f)
 
     train = DataLoader(Affectdataset(alldata['train'], flatten_time_series), \
-        shuffle=train_shuffle, num_workers=num_workers, batch_size=batch_size)
+        shuffle=train_shuffle, num_workers=num_workers, batch_size=batch_size, \
+        collate_fn=process)
     valid = DataLoader(Affectdataset(alldata['valid'], flatten_time_series), \
-        shuffle=False, num_workers=num_workers, batch_size=batch_size)
+        shuffle=False, num_workers=num_workers, batch_size=batch_size, \
+        collate_fn=process)
     test = DataLoader(Affectdataset(alldata['test'], flatten_time_series), \
-        shuffle=False, num_workers=num_workers, batch_size=batch_size)
+        shuffle=False, num_workers=num_workers, batch_size=batch_size, \
+        collate_fn=process)
 
     return train, valid, test
+
+
+def process(inputs:List):
+    processed_input = []
+    processed_input_lengths = []
+    labels = []
+    
+    for i in range(len(inputs[0])-1):
+        feature = []
+        for sample in inputs:
+            feature.append(sample[i])
+        processed_input_lengths.append(torch.as_tensor([v.size(0) for v in feature]))
+        processed_input.append(pad_sequence(feature, batch_first=True))
+    
+    for sample in inputs:
+        labels.append(sample[-1])
+
+    return processed_input, processed_input_lengths, torch.tensor(labels).view(len(inputs), 1)
--- training_structures/Simple_Late_Fusion.py
+++ training_structures/Simple_Late_Fusion.py
@@ -9,39 +9,53 @@ from objective_functions.regularization import RegularizationLoss
 softmax = nn.Softmax()
 
 class MMDL(nn.Module):
-    def __init__(self,encoders,fusion,head):
+    def __init__(self,encoders,fusion,head,has_padding=False):
         super(MMDL,self).__init__()
         self.encoders = nn.ModuleList(encoders)
         self.fuse = fusion
         self.head = head
+        self.has_padding=has_padding
     
     def forward(self,inputs,training=False):
         outs = []
-        for i in range(len(inputs)):
-            outs.append(self.encoders[i](inputs[i], training=training))
+        if self.has_padding:
+            for i in range(len(inputs[0])):
+                outs.append(self.encoders[i]([inputs[0][i],inputs[1][i]], training=training))
+        else:
+            for i in range(len(inputs)):
+                outs.append(self.encoders[i](inputs[i], training=training))
         out = self.fuse(outs, training=training)
         return self.head(out, training=training)
 
+
 def train(
-    encoders,fusion,head,train_dataloader,valid_dataloader,
-    total_epochs,optimtype=torch.optim.RMSprop,lr=0.001,weight_decay=0.0,
+    encoders,fusion,head,train_dataloader,valid_dataloader,total_epochs,
+    task="classification",optimtype=torch.optim.RMSprop,lr=0.001,weight_decay=0.0,
     criterion=nn.CrossEntropyLoss(),regularization=False,auprc=False,save='best.pt'):
     
-    model = MMDL(encoders,fusion,head).cuda()
+    model = MMDL(encoders,fusion,head,regularization).cuda()
     op = optimtype(model.parameters(),lr=lr,weight_decay=weight_decay)
     bestvalloss = 10000
+    
     if regularization:
         regularize = RegularizationLoss(criterion, model)
+    
     for epoch in range(total_epochs):
         totalloss = 0.0
         totals = 0
+        model.train()
         for j in train_dataloader:
+            #print([i for i in j[:-1]])
             op.zero_grad()
-            out=model([i.float().cuda() for i in j[:-1]],training=True)
-            #print(j[-1])
-            loss=criterion(out,j[-1].cuda())
             if regularization:
-                loss += regularize(out, [i.float().cuda() for i in j[:-1]])
+                with torch.backends.cudnn.flags(enabled=False):
+                    out=model([[i.cuda() for i in j[0]], j[1]],training=True)
+                    #print(j[-1])
+                    loss=criterion(out,j[-1].cuda()) + regularize(out, [[i.cuda() for i in j[0]], j[1]])
+            else:
+                out=model([i.float().cuda() for i in j[:-1]],training=True)
+                #print(j[-1])
+                loss=criterion(out,j[-1].cuda())
             totalloss += loss * len(j[-1])
             totals+=len(j[-1])
             if regularization:
@@ -50,25 +64,34 @@ def train(
                 loss.backward()
             op.step()
         print("Epoch "+str(epoch)+" train loss: "+str(totalloss/totals))
+        
+        model.eval()
         with torch.no_grad():
             totalloss = 0.0
             totals = 0
             correct = 0
             pts = []
             for j in valid_dataloader:
-                out = model([i.float().cuda() for i in j[:-1]],training=False)
+                if regularization:
+                    out=model([[i.cuda() for i in j[0]], j[1]],training=False)
+                else:
+                    out = model([i.float().cuda() for i in j[:-1]],training=False)
                 loss = criterion(out,j[-1].cuda())
                 totalloss += loss*len(j[-1])
                 for i in range(len(j[-1])):
                     totals += 1
-                    if torch.argmax(out[i]).item() == j[-1][i].item():
-                        correct += 1
+                    if task == "classification":
+                        if torch.argmax(out[i]).item() == j[-1][i].item():
+                            correct += 1
                     if auprc:
                         #pdb.set_trace()
                         sm=softmax(out[i])
                         pts.append((sm[1].item(), j[-1][i].item()))
         valloss=totalloss/totals
-        print("Epoch "+str(epoch)+" valid loss: "+str(valloss)+" acc: "+str(float(correct)/totals))
+        if task == "classification":
+            print("Epoch "+str(epoch)+" valid loss: "+str(valloss)+" acc: "+str(float(correct)/totals))
+        elif task == "regression":
+            print("Epoch "+str(epoch)+" valid loss: "+str(valloss))
         if auprc:
             print("AUPRC: "+str(AUPRC(pts)))
         if valloss<bestvalloss:
@@ -76,21 +99,34 @@ def train(
             print("Saving Best")
             torch.save(model,save)
 
-def test(model,test_dataloader,auprc=False):
+def test(
+    model,test_dataloader,criterion=nn.CrossEntropyLoss(),
+    task="classification",regularization=False,auprc=False):
     with torch.no_grad():
         totals=0
         correct=0
+        totalloss = 0.0
         pts=[]
         for j in test_dataloader:
-            out=model([i.float().cuda() for i in j[:-1]])
+            if regularization:
+                out=model([[i.cuda() for i in j[0]], j[1]],training=False)
+            else:
+                out = model([i.float().cuda() for i in j[:-1]],training=False)
+            loss = criterion(out,j[-1].cuda())
+            totalloss += loss*len(j[-1])
             for i in range(len(j[-1])):
                 totals += 1
-                if torch.argmax(out[i]).item()==j[-1][i].item():
-                    correct += 1
+                if task == "classification":
+                    if torch.argmax(out[i]).item()==j[-1][i].item():
+                        correct += 1
                 if auprc:
                     sm=softmax(out[i])
                     pts.append((sm[1].item(),j[-1][i].item()))
-        print("acc: "+str(float(correct)/totals))
+        testloss=totalloss/totals
+        if task == "classification":
+            print("acc: "+str(float(correct)/totals))
+        elif task == "regression":
+            print("mse: "+str(testloss))
         if auprc:
             print("AUPRC: "+str(AUPRC(pts)))
 
