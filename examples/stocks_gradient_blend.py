Fix stocks gradient blend

examples/stocks_gradient_blend.py
@@ -9,14 +9,11 @@ from torch import nn
 from datasets.stocks.get_data import get_dataloader
 from fusions.common_fusions import ConcatWithLinear
 from modules.transformer import TransformerEncoder
-from training_structures.gradient_blend import train
+from training_structures.gradient_blend import train, test
 
 
 stocks = sorted(['MCD', 'SBUX', 'HSY', 'HRL'])
 train_loader, val_loader, test_loader = get_dataloader(stocks, stocks, ['MCD'], modality_first=True, cuda=False)
-train_loader = train_loader
-val_loader = val_loader
-test_loader = test_loader
 
 criterion = nn.MSELoss()
 
@@ -34,29 +31,37 @@ class EarlyFusion(nn.Module):
         self.lstm = nn.LSTM(n_features, self.hidden_size, batch_first=True)
         self.fnn = nn.Linear(self.hidden_size, 1)
 
-    def forward(self, x):
+    def forward(self, x, training=True):
+        if len(x.shape) == 2:
+            # unimodal
+            x = x.reshape([x.shape[0], x.shape[1], 1])
+
         _, (h, _) = self.lstm(x,
                               (self.init_hidden.repeat(x.shape[0]).reshape(1, x.shape[0], -1),
                                self.init_cell.repeat(x.shape[0]).reshape(1, x.shape[0], -1)))
-        return self.fnn(h.reshape(-1, self.hidden_size))
+        out = self.fnn(h.reshape(-1, self.hidden_size))
+        out = out.reshape(out.shape[:1])
+        return out
 
 class EarlyFusionFuse(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x):
-        x = torch.tensor(x).cuda()
+    def forward(self, x, training=True):
+        x = torch.stack(x)
         x = x.permute([1, 2, 0])
         return x
 
 def do_train():
-    unimodal_models = [nn.Identity() for x in stocks]
-    multimodal_classification_head = EarlyFusion(len(stocks))
-    unimodal_classification_heads = [EarlyFusion(1) for x in stocks]
-    fuse = EarlyFusionFuse()
+    unimodal_models = [nn.Identity().cuda() for x in stocks]
+    multimodal_classification_head = EarlyFusion(len(stocks)).cuda()
+    unimodal_classification_heads = [EarlyFusion(1).cuda() for x in stocks]
+    fuse = EarlyFusionFuse().cuda()
+    training_structures.gradient_blend.criterion = criterion
+
     train(unimodal_models,  multimodal_classification_head,
           unimodal_classification_heads, fuse, train_dataloader=train_loader, valid_dataloader=val_loader,
-          num_epoch=80, lr=0.001, optimtype=torch.optim.Adam)
+          classification=False, gb_epoch=2, num_epoch=4, lr=0.001, optimtype=torch.optim.Adam)
 
 #train
 do_train()
@@ -64,9 +69,4 @@ do_train()
 #test
 model = torch.load('best.pt').cuda()
 model.eval()
-losses = []
-for j, (x, y) in enumerate(test_loader):
-    out = model(x)
-    loss = criterion(out, y)
-    losses.append(loss.item() * len(out))
-print(f'Test loss: {np.sum(losses) / len(test_loader.dataset)}')
+test(model, test_loader, classification=False)