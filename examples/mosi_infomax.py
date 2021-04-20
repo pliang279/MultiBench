d98b179d629a0a14d3cd539c7b7478f530757a5e
Wu Zetian <ztwu_nil@zju.edu.cn>
2021-04-19 23:18:53 -0700

    add contrastive learning

--- examples/mosi_infomax.py
+++ examples/mosi_infomax.py
@@ -11,7 +11,7 @@ from unimodals.common_models import GRU, MLP
 
 
 traindata, validdata, testdata = get_dataloader('../affect/processed/mosi_data.pkl')
-'''
+
 encoders=[GRU(20,50,dropout=True,has_padding=True).cuda(), \
     GRU(5,15,dropout=True,has_padding=True).cuda(),\
     GRU(300,600,dropout=True,has_padding=True).cuda()]
@@ -19,9 +19,9 @@ head=MLP(665,300,1).cuda()
 fusion=Concat().cuda()
 
 train(encoders,fusion,head,traindata,validdata,1000,True,True, \
-    task="regression",optimtype=torch.optim.AdamW,lr=1e-4,save='best2.pt', \
-    weight_decay=0.01,criterion=torch.nn.MSELoss(),regularization=False)
-'''
+    task="regression",optimtype=torch.optim.AdamW,lr=1e-4,save='best_mae.pt', \
+    weight_decay=0.01,criterion=torch.nn.L1Loss(),regularization=False)
+
 print("Testing:")
-model=torch.load('best2.pt').cuda()
-test(model,testdata,True,torch.nn.MSELoss(),"regression",)
+model=torch.load('best_mae.pt').cuda()
+test(model,testdata,True,torch.nn.L1Loss(),"regression",)
---
+++ training_structures/Contrastive_Learning.py
@@ -0,0 +1,186 @@
+from tqdm import tqdm
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.optim.lr_scheduler import ExponentialLR
+
+from utils.AUPRC import AUPRC
+from objective_functions.contrast import NCEAverage, NCECriterion
+#import pdb
+
+
+softmax = nn.Softmax()
+
+
+class MMDL(nn.Module):
+    def __init__(self,encoders,fusion,head,has_padding=False):
+        super(MMDL,self).__init__()
+        self.encoders = nn.ModuleList(encoders)
+        self.fuse = fusion
+        self.head = head
+        self.has_padding=has_padding
+        self.contrast = NCEAverage(50, 600, 7, 16384)
+    
+    def forward(self,inputs,classifier=False,training=False):
+        outs = []
+        if self.has_padding:
+            for i in range(len(inputs[0])):
+                outs.append(self.encoders[i]([inputs[0][i],inputs[1][i]], training=training))
+        else:
+            for i in range(len(inputs)):
+                outs.append(self.encoders[i](inputs[i], training=training))
+        
+        if classifier:
+            out = self.fuse(outs, training=training)
+            return self.head(out, training=training)
+        else:
+            out1, out2 = self.contrast(outs[0], outs[1], inputs[2])
+            return out1, out2 
+        
+
+def train(
+    encoders,train_dataloader,valid_dataloader,total_epochs,is_packed=False,
+    early_stop=False,optimtype=torch.optim.RMSprop,lr=0.001,weight_decay=0.0,
+    criterion=nn.CrossEntropyLoss(),auprc=False,save='best.pt'):
+    
+    model = MMDL(encoders,is_packed).cuda()
+    op = optimtype(model.parameters(),lr=lr,weight_decay=weight_decay)
+    scheduler = ExponentialLR(op, 0.9)
+    bestvalloss = 10000
+    patience = 0
+    contrast_criterion = NCECriterion(len(train_dataloader))
+
+    for epoch in range(total_epochs):
+        totalloss = 0.0
+        lastloss = 0.0
+        totals = 0
+        model.train()
+        
+        #Representation Learning
+        for j in train_dataloader:
+            #print([i for i in j[:-1]])
+            op.zero_grad()
+            if is_packed:
+                with torch.backends.cudnn.flags(enabled=False):
+                    out1, out2=model(
+                        [[j[0][0].cuda(), j[0][-1].cuda()], j[1], j[2].cuda()],training=True)
+                    #print(j[-1])
+                    loss1=contrast_criterion(out1)
+                    loss2=contrast_criterion(out2)
+                    loss = loss1+loss2
+            else:
+                out=model([i.float().cuda() for i in j[:-1]],training=True)
+                #print(j[-1])
+                loss=criterion(out,j[-1].cuda())
+            totalloss += loss * len(j[-1])
+            totals+=len(j[-1])
+            
+            loss.backward()
+            op.step()
+        
+        train_loss = totalloss/totals
+        print("Epoch "+str(epoch)+" train loss: "+str(train_loss))
+
+        if lastloss == 0.0:
+            lastloss = train_loss
+            continue
+
+        if (lastloss-train_loss)/lastloss < 1e-6:
+            patience += 1
+        else:
+            patience = 0
+        if patience > 7:
+            print("Early Stop!")
+            break
+
+        lastloss = train_loss
+        
+    for epoch in range(total_epochs):
+        totalloss = 0.0
+        lastloss = 0.0
+        totals = 0
+        model.train()
+        for j in train_dataloader:
+            #print([i for i in j[:-1]])
+            op.zero_grad()
+            if is_packed:
+                with torch.backends.cudnn.flags(enabled=False):
+                    out=model([[i.cuda() for i in j[0]], j[1]],True,training=True)
+                    #print(j[-1])
+                    loss=criterion(out,j[-1].cuda())
+            else:
+                out=model([i.float().cuda() for i in j[:-1]],True,training=True)
+                #print(j[-1])
+                loss=criterion(out,j[-1].cuda())
+            totalloss += loss * len(j[-1])
+            totals+=len(j[-1])
+            
+            loss.backward()
+            op.step()
+        print("Epoch "+str(epoch)+" train loss: "+str(totalloss/totals))
+        model.eval()
+        with torch.no_grad():
+            totalloss = 0.0
+            totals = 0
+            correct = 0
+            pts = []
+            for j in valid_dataloader:
+                if is_packed:
+                    out=model([[i.cuda() for i in j[0]], j[1]],True,training=False)
+                else:
+                    out = model([i.float().cuda() for i in j[:-1]],True,training=False)
+                loss = criterion(out,j[-1].cuda())
+                totalloss += loss*len(j[-1])
+                for i in range(len(j[-1])):
+                    totals += 1
+                    if torch.argmax(out[i]).item() == j[-1][i].item():
+                        correct += 1
+                    if auprc:
+                        #pdb.set_trace()
+                        sm=softmax(out[i])
+                        pts.append((sm[1].item(), j[-1][i].item()))
+        valloss=totalloss/totals
+        print("Epoch "+str(epoch)+" valid loss: "+str(valloss)+" acc: "+str(float(correct)/totals))
+        if auprc:
+            print("AUPRC: "+str(AUPRC(pts)))
+        if valloss<bestvalloss:
+            patience = 0
+            bestvalloss=valloss
+            print("Saving Best")
+            torch.save(model,save)
+        else:
+            patience += 1
+        if early_stop and patience > 20:
+            break
+        
+        scheduler.step()
+
+
+def test(
+    model,test_dataloader,is_packed=False,criterion=nn.CrossEntropyLoss(), auprc=False):
+    with torch.no_grad():
+        totals=0
+        correct=0
+        totalloss = 0.0
+        pts=[]
+        for j in test_dataloader:
+            if is_packed:
+                out=model([[i.cuda() for i in j[0]], j[1]],training=False)
+            else:
+                out = model([i.float().cuda() for i in j[:-1]],training=False)
+            loss = criterion(out,j[-1].cuda())
+            #print(torch.cat([out,j[-1].cuda()],dim=1))
+            totalloss += loss*len(j[-1])
+            for i in range(len(j[-1])):
+                totals += 1
+                if torch.argmax(out[i]).item()==j[-1][i].item():
+                    correct += 1
+                if auprc:
+                    sm=softmax(out[i])
+                    pts.append((sm[1].item(),j[-1][i].item()))
+        testloss=totalloss/totals
+        print("acc: "+str(float(correct)/totals))
+        if auprc:
+            print("AUPRC: "+str(AUPRC(pts)))
+
