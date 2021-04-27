ce5eb5c9bea2467874a259089f05b6cc01458592
lvyiwei1 <ylyu1@andrew.cmu.edu>
2021-04-27 17:35:41 -0400

    Added mimic tensor matrix

--- examples/mimic_tensor_matrix.py
+++ examples/mimic_tensor_matrix.py
@@ -2,19 +2,19 @@ import sys
 import os
 sys.path.append(os.getcwd())
 from training_structures.Simple_Late_Fusion import train, test
-from fusions.common_fusions import Concat
+from fusions.common_fusions import MultiplicativeInteractions2Modal
 from datasets.mimic.get_data import get_dataloader
 from unimodals.common_models import MLP, GRU
 from torch import nn
 import torch
 
 #get dataloader for icd9 classification task 7
-traindata, validdata, testdata = get_dataloader(7, imputed_path='datasets/mimic/im.pk')
+traindata, validdata, testdata = get_dataloader(-1, imputed_path='datasets/mimic/im.pk')
 
 #build encoders, head and fusion layer
-encoders = [MLP(5, 10, 10,dropout=False).cuda(), GRU(12, 30,dropout=False).cuda()]
-head = MLP(730, 40, 2, dropout=False).cuda()
-fusion = Concat().cuda()
+encoders = [MLP(5, 10, 10,dropout=True).cuda(), GRU(12, 30,dropout=True).cuda()]
+head = MLP(100,40, 2, dropout=True).cuda()
+fusion = MultiplicativeInteractions2Modal([10,720],100,'matrix',flatten=True)
 
 #train
 train(encoders, fusion, head, traindata, validdata, 20, auprc=True)
