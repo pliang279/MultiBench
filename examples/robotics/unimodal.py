40ab7e68086ebbd1c684620e200e28d6b05db146
Xiang Fan <sfanxiang@gmail.com>
2021-05-19 18:26:26 +0800

    Make robotics use all in one train

--- examples/robotics/unimodal.py
+++ examples/robotics/unimodal.py
@@ -19,6 +19,7 @@ from unimodals.robotics.encoders import (
 )
 from unimodals.robotics.decoders import ContactDecoder
 from training_structures.unimodal import train, test
+from private_test_scripts.all_in_one import all_in_one_train,all_in_one_test
 from robotics_utils import set_seeds
 
 from datasets.robotics.data_loader import get_data
@@ -57,20 +58,21 @@ class selfsupervised:
             raise Exception(f'encoder {uniencoder} not found')
         self.head = ContactDecoder(z_dim=2 * configs["zdim"], deterministic=configs["deterministic"])
 
+        self.allmodules = [self.encoder, self.head]
+
         self.optimtype = optim.Adam
-
-        # losses
         self.loss_contact_next = nn.BCEWithLogitsLoss()
-
         self.train_loader, self.val_loader = get_data(self.device, self.configs)
 
     def train(self):
-        train(self.encoder, self.head,
-              self.train_loader, self.val_loader,
-              self.configs['max_epoch'],
-              optimtype=self.optimtype,
-              lr=self.configs['lr'],
-              criterion=self.loss_contact_next)
+        def trainprocess():
+            train(self.encoder, self.head,
+                  self.train_loader, self.val_loader,
+                  self.configs['max_epoch'],
+                  optimtype=self.optimtype,
+                  lr=self.configs['lr'],
+                  criterion=self.loss_contact_next)
+        all_in_one_train(trainprocess, self.allmodules)
 
 with open('examples/robotics/training_default.yaml') as f:
     configs = yaml.load(f)
