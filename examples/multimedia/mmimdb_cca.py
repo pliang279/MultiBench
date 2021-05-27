4894b7e89a9e428aec32772364e48effa6f9601d
Wu Zetian <ztwu_nil@zju.edu.cn>
2021-05-26 22:46:47 -0700

    fix cca and add unimodal for mmimdb

--- examples/multimedia/mmimdb_cca.py
+++ examples/multimedia/mmimdb_cca.py
@@ -7,18 +7,19 @@ import torch
 from training_structures.cca import train, test
 from fusions.common_fusions import Concat
 from datasets.imdb.get_data import get_dataloader
-from unimodals.common_models import MLP, VGG16, Linear
+from unimodals.common_models import MLP, VGG16, Linear,
 
-traindata, validdata, testdata = get_dataloader('../video/multimodal_imdb.hdf5', vgg=True)
+traindata, validdata, testdata = get_dataloader('../video/multimodal_imdb.hdf5', vgg=True, batch_size=800)
 
-#encoders=[MLP(300, 512, 512), MLP(4096, 1000, 512)]
+outdim = 256
+encoders=[MLP(300,512,outdim), MLP(4096,1024,outdim)]
 #encoders=[MLP(300, 512, 512), VGG16(512)]
-encoders=[Linear(300, 512), Linear(4096,512)]
-head=MLP(1024,512,23).cuda()
+#encoders=[Linear(300, 512), Linear(4096,512)]
+head=MLP(20,64,23).cuda()
 fusion=Concat().cuda()
 
-train(encoders,fusion,head,traindata,validdata,1000, early_stop=True,task="multilabel",\
-    save="best_cca.pt", optimtype=torch.optim.RMSprop,lr=1e-10,weight_decay=0, criterion=torch.nn.BCEWithLogitsLoss())
+train(encoders,fusion,head,traindata,validdata,1000, early_stop=True,task="multilabel",outdim=outdim,\
+    save="best_cca.pt", optimtype=torch.optim.RMSprop,lr=1e-3,weight_decay=1e-5, criterion=torch.nn.BCEWithLogitsLoss())
 
 print("Testing:")
 model=torch.load('best_cca.pt').cuda()
