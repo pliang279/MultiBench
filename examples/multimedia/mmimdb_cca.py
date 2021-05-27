3fe9b5457f0606f6b17381e6fe7fd0feb45789aa
Wu Zetian <ztwu_nil@zju.edu.cn>
2021-05-27 16:31:11 -0700

    finalize cca

--- examples/multimedia/mmimdb_cca.py
+++ examples/multimedia/mmimdb_cca.py
@@ -4,18 +4,18 @@ sys.path.append(os.getcwd())
 
 import torch
 
-from training_structures.cca import train, test
+from training_structures.cca_onestage import train, test
 from fusions.common_fusions import Concat
 from datasets.imdb.get_data import get_dataloader
 from unimodals.common_models import MLP, VGG16, Linear
 
 traindata, validdata, testdata = get_dataloader('../video/multimodal_imdb.hdf5', vgg=True, batch_size=800)
 
-outdim = 256
+outdim = 32
 encoders=[MLP(300,512,outdim), MLP(4096,1024,outdim)]
 #encoders=[MLP(300, 512, 512), VGG16(512)]
 #encoders=[Linear(300, 512), Linear(4096,512)]
-head=MLP(20,64,23).cuda()
+head=MLP(2*outdim,2*outdim,23).cuda()
 fusion=Concat().cuda()
 
 train(encoders,fusion,head,traindata,validdata,1000, early_stop=True,task="multilabel",outdim=outdim,\
