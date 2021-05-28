adfb605575dcb7844feae0163df380fc21e6b9dd
Jason Wu <jsonwu@cmu.edu>
2021-05-28 04:16:43 +0000

    add enrico cca example

---
+++ examples/hci/enrico_cca.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from torch import nn
+sys.path.append(os.getcwd())
+from training_structures.cca import train, test
+from fusions.common_fusions import Concat
+from datasets.enrico.get_data import get_dataloader
+from unimodals.common_models import VGG16, VGG16Slim,DAN,Linear,MLP, VGG11Slim, VGG11Pruned
+
+import torch
+
+dls, weights = get_dataloader('datasets/enrico/dataset')
+traindata, validdata, testdata = dls
+criterion = nn.CrossEntropyLoss(weight=torch.tensor(weights)).cuda()
+# encoders=[VGG16Slim(64).cuda(), DAN(4, 16, dropout=True, dropoutp=0.25).cuda(), DAN(28, 16, dropout=True, dropoutp=0.25).cuda()]
+# head = Linear(96, 20)
+encoders=[VGG11Slim(16, dropout=True, dropoutp=0.2, freeze_features=True).cuda(), VGG11Slim(16, dropout=True, dropoutp=0.2, freeze_features=True).cuda()]
+# encoders = [DAN(4, 16, dropout=True, dropoutp=0.25).cuda(), DAN(28, 16, dropout=True, dropoutp=0.25).cuda()]
+head = Linear(32, 20).cuda()
+
+fusion=Concat().cuda()
+
+train(encoders,fusion,head,traindata,validdata,50,optimtype=torch.optim.Adam,lr=0.0001,weight_decay=0)
+
+print("Testing:")
+model=torch.load('best.pt').cuda()
+test(model,testdata)
+
+
