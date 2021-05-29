a1c3ea0d2950cdf2e6ca0382bd0f6b1678553e3d
Xiang Fan <sfanxiang@gmail.com>
2021-05-29 18:42:56 -0400

    Fix gentle_push

--- examples/gentle_push/LF.py
+++ examples/gentle_push/LF.py
@@ -16,12 +16,13 @@ from torch.utils.data import DataLoader
 
 from datasets.gentle_push.data_loader import Dataset, PushTask
 from unimodals.robotics.encoders import (
-    ProprioEncoder, ForceEncoder, GentlePushImageEncoder, DepthEncoder, ActionEncoder,
+    GentlePushImageEncoder, GentlePushLinearEncoder,
 )
 from unimodals.common_models import MLP
 from unimodals.robotics.decoders import ContactDecoder
 from fusions.common_fusions import Concat
 from training_structures.Simple_Late_Fusion import train, test
+from private_test_scripts.all_in_one import all_in_one_train, all_in_one_test
 
 Task = PushTask
 
@@ -36,6 +37,7 @@ fannypack.data.set_cache_path('datasets/gentle_push/cache')
 # Load trajectories into memory
 train_trajectories = Task.get_train_trajectories(**dataset_args)
 val_trajectories = Task.get_eval_trajectories(**dataset_args)
+test_trajectories = Task.get_test_trajectories(**dataset_args)
 
 train_loader = DataLoader(
     Dataset(train_trajectories),
@@ -47,23 +49,36 @@ val_loader = DataLoader(
     batch_size=32,
     shuffle=True,
 )
+test_loader = DataLoader(
+    Dataset(test_trajectories),
+    batch_size=32,
+    shuffle=False,
+)
 
 encoders = [
-    ProprioEncoder(32, input_dim=2, alpha=1.0),
-    ProprioEncoder(32, input_dim=3, alpha=1.0),
-    ProprioEncoder(32, input_dim=7, alpha=1.0),
+    GentlePushLinearEncoder(32, input_dim=2, alpha=1.0),
+    GentlePushLinearEncoder(32, input_dim=3, alpha=1.0),
+    GentlePushLinearEncoder(32, input_dim=7, alpha=1.0),
     GentlePushImageEncoder(32, alpha=1.0),
-    ProprioEncoder(32, input_dim=7, alpha=1.0),
+    GentlePushLinearEncoder(32, input_dim=7, alpha=1.0),
 ]
 fusion = Concat()
 head=MLP(320,128,2)
+allmodules = [*encoders, fusion, head]
 optimtype = optim.Adam
 loss_state = nn.MSELoss()
 
-train(encoders, fusion, head,
-      train_loader, val_loader,
-      15,
-      task='regression',
-      optimtype=optimtype,
-      criterion=loss_state,
-      lr=0.001)
+def trainprocess():
+    train(encoders, fusion, head,
+          train_loader, val_loader,
+          10,
+          task='regression',
+          optimtype=optimtype,
+          criterion=loss_state,
+          lr=0.00001)
+all_in_one_train(trainprocess, allmodules)
+
+model = torch.load('best.pt').cuda()
+def testprocess():
+    test(model, test_loader, task='regression', criterion=loss_state)
+all_in_one_test(testprocess, [model])
