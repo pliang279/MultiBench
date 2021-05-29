ec501f1e49e7d8f19dce53f4c3ee8590009c74e7
Wu Zetian <ztwu_nil@zju.edu.cn>
2021-05-28 19:30:58 -0700

    separate rmfe from baseline

--- examples/multimedia/mmimdb_unimodal.py
+++ examples/multimedia/mmimdb_unimodal.py
@@ -10,14 +10,15 @@ from unimodals.common_models import MLP
 
 traindata, validdata, testdata = get_dataloader('../video/multimodal_imdb.hdf5', vgg=True)
 
-encoders=MLP(300, 512, 512).cuda()
+#encoders=MLP(300, 512, 512).cuda()
+encoders=MLP(4096, 1024, 512).cuda()
 #encoders=[MLP(300, 512, 512), VGG16(512)]
 head=MLP(512,256,23).cuda()
 
-train(encoders,head,traindata,validdata,1000, early_stop=True,task="multilabel", save_encoder="encoder_t.pt", modalnum=0,\
-    save_head="head_text.pt", optimtype=torch.optim.AdamW,lr=5e-5,weight_decay=0.01, criterion=torch.nn.BCEWithLogitsLoss())
+train(encoders,head,traindata,validdata,1000, early_stop=True,task="multilabel", save_encoder="encoder_i.pt", modalnum=1,\
+    save_head="head_i.pt", optimtype=torch.optim.AdamW,lr=5e-5,weight_decay=0.01, criterion=torch.nn.BCEWithLogitsLoss())
 
 print("Testing:")
-encoder=torch.load('encoder_text.pt').cuda()
-head=torch.load('head_text.pt').cuda()
-test(encoder,head,testdata,criterion=torch.nn.BCEWithLogitsLoss(),task="multilabel")
+encoder=torch.load('encoder_i.pt').cuda()
+head=torch.load('head_i.pt').cuda()
+test(encoder,head,testdata,task="multilabel", modalnum=1)
