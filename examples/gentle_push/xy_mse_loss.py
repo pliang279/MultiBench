aa532115baa1b98fb6375533fd231d2c3fe37d4b
Xiang Fan <sfanxiang@gmail.com>
2021-05-31 22:03:29 -0400

    gentle_push: xy_mse_loss: Fix slicing

--- examples/gentle_push/xy_mse_loss.py
+++ examples/gentle_push/xy_mse_loss.py
@@ -8,7 +8,7 @@ class XYMSELoss(nn.Module):
         self.criterion = nn.MSELoss()
 
     def forward(self, input, target):
-        x_loss = self.criterion(input[:, 0], target[:, 0])
-        y_loss = self.criterion(input[:, 1], target[:, 1])
+        x_loss = self.criterion(input[:, :, 0], target[:, :, 0])
+        y_loss = self.criterion(input[:, :, 1], target[:, :, 1])
         xy_loss = self.criterion(input, target)
         return torch.stack([x_loss, y_loss, xy_loss])
