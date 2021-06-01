8f4aca75329972f1c2243cbb29149e4f32fb318e
Xiang Fan <sfanxiang@gmail.com>
2021-05-31 21:23:07 -0400

    gentle_push: Remove unused dependency

--- examples/gentle_push/mult.py
+++ examples/gentle_push/mult.py
@@ -19,7 +19,6 @@ from torch.utils.data import DataLoader
 from datasets.gentle_push.data_loader import SubsequenceDataset, PushTask
 from unimodals.common_models import Sequential, Transpose, Reshape, Identity
 from unimodals.gentle_push.head import Head
-from fusions.common_fusions import ConcatWithLinear
 from fusions.mult import MULTModel
 from training_structures.Simple_Late_Fusion import train, test
 from private_test_scripts.all_in_one import all_in_one_train, all_in_one_test
