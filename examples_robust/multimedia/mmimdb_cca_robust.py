0da8547d447e301f5ed4b0a600cd0bc477647a92
kapikantzari <yuncheng@andrew.cmu.edu>
2021-06-03 01:04:08 -0400

    first draft

--- examples_robust/multimedia/mmimdb_cca_robust.py
+++ examples_robust/multimedia/mmimdb_cca_robust.py
@@ -1,33 +1,33 @@
-
 import sys
 import os
-sys.path.append('/home/pliang/multibench/MultiBench/datasets/imdb')
-sys.path.append('/home/pliang/multibench/MultiBench')
-
-import torch
+sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
 
 import torch
 
 from training_structures.cca_onestage import train, test
 from fusions.common_fusions import Concat
+sys.path.append('/home/pliang/multibench/MultiBench/datasets/imdb')
 from get_data_robust import get_dataloader, get_dataloader_robust
-from unimodals.common_models import MLP, VGG16, Linear
 from robustness.all_in_one import general_train, general_test
+from unimodals.common_models import MLP, VGG16, Linear, MaxOut_MLP
 
 traindata, validdata = get_dataloader('../../../video/multimodal_imdb.hdf5', vgg=True, batch_size=800)
-robustdata = get_dataloader_robust('../../../video/mmimdb', batch_size=800)
+robust_vision, robust_text = get_dataloader_robust('../../../video/mmimdb', batch_size=800)
 
-outdim = 32
-encoders=[MLP(300,512,outdim), MLP(4096,1024,outdim)]
+outdim = 256
+encoders=[MaxOut_MLP(512, 512, 300, outdim, False), MaxOut_MLP(512, 1024, 4096, outdim, False)]
+#encoders=[MLP(300,512,outdim), MLP(4096,1024,outdim)]
 #encoders=[MLP(300, 512, 512), VGG16(512)]
 #encoders=[Linear(300, 512), Linear(4096,512)]
-head=MLP(2*outdim,2*outdim,23).cuda()
+#head=MLP(2*outdim,2*outdim,23).cuda()
+head=Linear(2*outdim, 23).cuda()
 fusion=Concat().cuda()
 
 def trainprocess(filename):
-    train(encoders,fusion,head,traindata,validdata,1000, early_stop=True,task="multilabel",outdim=outdim,save=filename,optimtype=torch.optim.RMSprop,lr=1e-3,weight_decay=1e-5, criterion=torch.nn.BCEWithLogitsLoss())
+    train(encoders,fusion,head,traindata,validdata,1000, early_stop=True,task="multilabel",outdim=outdim,\
+    save=filename, optimtype=torch.optim.AdamW,lr=1e-2,weight_decay=0.01, criterion=torch.nn.BCEWithLogitsLoss())
 filename = general_train(trainprocess, 'mmimdb_cca')
 
-def testprocess(model, noise_level):
-    return test(model,robustdata[noise_level],criterion=torch.nn.BCEWithLogitsLoss(),task="multilabel")
-general_test(testprocess, filename, len(robustdata), multi_measure=True)
+def testprocess(model, robustdata):
+    return test(model, robustdata,criterion=torch.nn.BCEWithLogitsLoss(),task="multilabel")
+general_test(testprocess, filename, [robust_vision, robust_text], multi_measure=True)
--- examples_robust/multimedia/mmimdb_unimodal_robust.py
+++ examples_robust/multimedia/mmimdb_unimodal_robust.py
@@ -1,26 +1,27 @@
 import sys
 import os
-sys.path.append('/home/pliang/multibench/MultiBench/datasets/imdb')
-sys.path.append('/home/pliang/multibench/MultiBench')
+sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
 
 import torch
 
 from training_structures.unimodal import train, test
+sys.path.append('/home/pliang/multibench/MultiBench/datasets/imdb')
 from get_data_robust import get_dataloader, get_dataloader_robust
-from unimodals.common_models import MLP
 from robustness.all_in_one import general_train, general_test
+from unimodals.common_models import MLP
 
 traindata, validdata = get_dataloader('../../../video/multimodal_imdb.hdf5', vgg=True)
-robustdata = get_dataloader_robust('../../../video/mmimdb')
+robust_vision, robust_text = get_dataloader_robust('../../../video/mmimdb')
 
 encoders=MLP(300, 512, 512).cuda()
-#encoders=[MLP(300, 512, 512), VGG16(512)]
-head=MLP(512,256,23).cuda()
+#encoders=MLP(4096,1024,512).cuda()
+head=MLP(512,512,23).cuda()
 
 def trainprocess(filename_encoder, filename_head):
-    train(encoders,head,traindata,validdata,1000, early_stop=True,task="multilabel", save_encoder=filename_encoder, modalnum=0, save_head=filename_head, optimtype=torch.optim.AdamW,lr=5e-5,weight_decay=0.01, criterion=torch.nn.BCEWithLogitsLoss())
+    train(encoders,head,traindata,validdata,1000, early_stop=True,task="multilabel", save_encoder=filename_encoder, modalnum=0,\
+    save_head=filename_head, optimtype=torch.optim.AdamW,lr=1e-4,weight_decay=0.01, criterion=torch.nn.BCEWithLogitsLoss())
 filename = general_train(trainprocess, 'mmimdb_unimodal', encoder=True)
 
-def testprocess(encoder, head, noise_level):
-    return test(encoder,head,robustdata[noise_level],criterion=torch.nn.BCEWithLogitsLoss(),task="multilabel")
-general_test(testprocess, filename, len(robustdata), multi_measure=True, encoder=True)
+def testprocess(encoder, head, robustdata):
+    return test(encoder, head, robustdata,task="multilabel", modalnum=0)
+general_test(testprocess, filename, [robust_vision, robust_text], multi_measure=True, encoder=True)
