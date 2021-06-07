fd6af6d57045713b462b97162745b04a3d7c4eac
Wu Zetian <ztwu_nil@zju.edu.cn>
2021-06-06 21:38:27 -0700

    add robustness test for mmimdb

--- examples/multimedia/mmimdb_low_rank_tensor.py
+++ examples/multimedia/mmimdb_low_rank_tensor.py
@@ -7,7 +7,7 @@ from datasets.imdb.get_data import get_dataloader
 from unimodals.common_models import MaxOut_MLP, Linear
 from torch import nn
 import torch
-filename = 'lowrank.pt'
+
 traindata, validdata, testdata = get_dataloader('../video/multimodal_imdb.hdf5', vgg=True, batch_size=128)
 encoders=[MaxOut_MLP(512, 512, 300, linear_layer=False), MaxOut_MLP(512, 1024, 4096, 512, False)]
 head= Linear(512, 23).cuda()
