6c6e9cf542cbf43f9a4fe726d48dd697396b5bb4
Wu Zetian <ztwu_nil@zju.edu.cn>
2021-06-06 21:38:27 -0700

    add robustness test for mmimdb

--- examples/multimedia/mmimdb_multi_interac_matrix.py
+++ examples/multimedia/mmimdb_multi_interac_matrix.py
@@ -7,7 +7,7 @@ from datasets.imdb.get_data import get_dataloader
 from unimodals.common_models import MaxOut_MLP, Linear
 from torch import nn
 import torch
-filename = 'lowrank.pt'
+
 traindata, validdata, testdata = get_dataloader('../video/multimodal_imdb.hdf5', vgg=True, batch_size=128)
 encoders=[MaxOut_MLP(512, 512, 300, linear_layer=False), MaxOut_MLP(512, 1024, 4096, 512, False)]
 head= Linear(1024, 23).cuda()
@@ -15,7 +15,7 @@ head= Linear(1024, 23).cuda()
 fusion=MultiplicativeInteractions2Modal([512,512],1024,'matrix').cuda()
 
 train(encoders,fusion,head,traindata,validdata,1000, early_stop=True,task="multilabel", regularization=False,\
-    save="best_mim.pt", optimtype=torch.optim.AdamW,lr=1e-2,weight_decay=0.01, criterion=torch.nn.BCEWithLogitsLoss())
+    save="best_mim.pt", optimtype=torch.optim.AdamW,lr=8e-3,weight_decay=0.01, criterion=torch.nn.BCEWithLogitsLoss())
 
 print("Testing:")
 model=torch.load('best_mim.pt').cuda()
