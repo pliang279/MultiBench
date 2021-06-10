c15afeea33044bbcd91e0f732820aef11d4b7ce4
Blair Chen <blairc@andrew.cmu.edu>
2021-06-10 18:16:15 -0400

    revamp things

---
+++ new_examples/healthcare/mimic_MFM.py
@@ -0,0 +1,31 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+from training_structures.Supervised_Learning import train,test
+from fusions.common_fusions import Concat
+from unimodals.MVAE import TSEncoder,TSDecoder
+from unimodals.common_models import MLP
+from torch import nn
+import torch
+from objective_functions.recon import sigmloss1d
+from objective_functions.objectives_for_supervised_learning import MFM_objective
+from datasets.mimic.get_data import get_dataloader
+from utils.helper_modules import Sequential2
+
+traindata, validdata, testdata = get_dataloader(7, imputed_path='/home/pliang/yiwei/im.pk',flatten_time_series=True)
+classes=2
+n_latent=200
+series_dim=12
+timestep=24
+fuse=Sequential2(Concat(),MLP(2*n_latent,n_latent,n_latent//2)).cuda()
+encoders=[MLP(5,20,n_latent).cuda(),TSEncoder(series_dim,30,n_latent,timestep,returnvar=False).cuda()]
+decoders=[MLP(n_latent,20,5).cuda(),TSDecoder(series_dim,30,n_latent,timestep).cuda()]
+intermediates=[MLP(n_latent,n_latent//2,n_latent//2).cuda(),MLP(n_latent,n_latent//2,n_latent//2).cuda()]
+head=MLP(n_latent//2,20,classes).cuda()
+argsdict = {'decoders':decoders,'intermediates':intermediates}
+additional_modules=decoders+intermediates
+objective=MFM_objective(2.0,[sigmloss1d,sigmloss1d],[1.0,1.0])
+
+train(encoders,fuse,head,traindata,validdata,20,additional_modules,objective=objective,objective_args_dict=argsdict)
+mvae=torch.load('best.pt')
+test(mvae,testdata)
---
+++ objective_functions/objectives_for_supervised_learning.py
@@ -0,0 +1,88 @@
+from objective_functions.recon import recon_weighted_sum,elbo_loss
+import torch
+from objective_functions.cca import CCALoss
+
+def criterioning(pred,truth,criterion):
+    if type(criterion)==torch.nn.CrossEntropyLoss:
+        return criterion(pred,truth.long().cuda())
+    elif type(criterion)==torch.nn.modules.loss.BCEWithLogitsLoss or type(criterion)==torch.nn.MSELoss:
+        return criterion(pred,truth.float().cuda())
+
+def MFM_objective(ce_weight,modal_loss_funcs,recon_weights,input_to_float=True,criterion=torch.nn.CrossEntropyLoss()):
+    recon_loss_func = recon_weighted_sum(modal_loss_funcs,recon_weights)
+    def actualfunc(pred,truth,args):
+        ints = args['intermediates']
+        reps = args['reps']
+        fused = args['fused']
+        decoders = args['decoders']
+        inps = args['inputs']
+        recons = []
+        for i in range(len(reps)):
+            recons.append(decoders[i](torch.cat([ints[i](reps[i]),fused],dim=1)))
+        ce_loss = criterioning(pred,truth,criterion)
+        if input_to_float:
+            inputs = [i.float().cuda() for i in inps]
+        else:
+            inputs = [i.cuda() for i in inps]
+        recon_loss = recon_loss_func(recons,inputs)
+        return ce_loss*ce_weight+recon_loss
+    return actualfunc
+
+def reparameterize(mu, logvar, training):
+    if training:
+        std = logvar.mul(0.5).exp_()
+        eps = torch.autograd.Variable(std.data.new(std.size()).normal_())
+        return eps.mul(std).add_(mu)
+    else:
+        return mu
+
+def MVAE_objective(ce_weight,modal_loss_funcs,recon_weights,input_to_float=True,annealing=1.0,criterion=torch.nn.CrossEntropyLoss()):
+    recon_loss_func = elbo_loss(modal_loss_funcs,recon_weights,annealing)
+    def allnonebuti(i,item):
+        ret=[None for w in modal_loss_funcs]
+        ret[i]=item
+        return ret
+    def actualfunc(pred,truth,args):
+        training = args['training']
+        reps = args['reps']
+        fusedmu,fusedlogvar = args['fused']
+        decoders = args['decoders']
+        inps = args['inputs']
+        reconsjoint = []
+        
+        if input_to_float:
+            inputs = [i.float().cuda() for i in inps]
+        else:
+            inputs = [i.cuda() for i in inps]
+        for i in range(len(inps)):
+            reconsjoint.append(decoders[i](reparameterize(fusedmu,fusedlogvar,training)))
+        total_loss = recon_loss_func(reconsjoint,inputs,fusedmu,fusedlogvar)
+        for i in range(len(inps)):
+            mu,logvar = reps[i]
+            recon = decoders[i](reparameterize(mu,logvar,training))
+            total_loss += recon_loss_func(allnonebuti(i,recon),allnonebuti(i,inputs[i]),mu,logvar)
+        total_loss += ce_weight * criterioning(pred,truth,criterion)
+        return total_loss
+    return actualfunc
+
+def CCA_objective(out_dim,cca_weight=0.001,criterion=torch.nn.CrossEntropyLoss()):
+    lossfunc = CCALoss(out_dim,False, device=torch.device("cuda"))
+    def actualfunc(pred,truth,args):
+        ce_loss = criterioning(pred,truth,criterion)
+        outs = args['reps']
+        cca_loss = lossfunc(outs[0],outs[1])
+        return cca_loss * cca_weight + ce_loss
+    return actualfunc
+
+def RefNet_objective(ref_weight,criterion=torch.nn.CrossEntropyLoss()):
+    ss_criterion=torch.nn.CosineEmbeddingLoss()
+    def actualfunc(pred,truth,args):
+        ce_loss = criterioning(pred,truth,criterion)
+        refiner = args['refiner']
+        fused = args['fused']
+        inps = args['inputs']
+        refinerout = refiner(fused)
+        inputsizes = [torch.flatten(t[0],start_dim=0).size(0) for t in inps]
+        ss_loss=0.0
+        for i in range(len(inps)):
+            out = refinerout[:,:]
