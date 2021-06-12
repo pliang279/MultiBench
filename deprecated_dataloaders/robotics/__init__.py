c405b80c3721f2e8229c3c37d70637eda3c1d4fa
kapikantzari <yuncheng@andrew.cmu.edu>
2021-06-12 02:10:46 -0400

    deprecate old dataloaders

---
+++ deprecated_dataloaders/robotics/__init__.py
@@ -0,0 +1,4 @@
+from .MultimodalManipulationDataset import MultimodalManipulationDataset
+from .MultimodalManipulationDataset_robust import MultimodalManipulationDataset_robust
+from .ProcessForce import ProcessForce
+from .ToTensor import ToTensor
