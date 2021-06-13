ea6b1af5f38ee0e53d923fb435f34a83ddf372b5
Xiang Fan <sfanxiang@gmail.com>
2021-06-13 16:08:51 -0400

    gentle_push: Readd robustness

--- examples/gentle_push/unimodal_sensors.py
+++ examples/gentle_push/unimodal_sensors.py
@@ -17,14 +17,18 @@ import unimodals.gentle_push.layers as layers
 from torch.utils.data import DataLoader
 
 from datasets.gentle_push.data_loader import SubsequenceDataset, PushTask
+from datasets.gentle_push.data_loader_robust import (
+    SubsequenceDataset as RobustSubsequenceDataset,
+    PushTask as RobustPushTask,
+)
 from unimodals.common_models import Sequential, Transpose, Reshape, MLP
 from unimodals.gentle_push.head import Head
 from fusions.common_fusions import ConcatWithLinear
 from training_structures.Supervised_Learning import train, test
-from private_test_scripts.all_in_one import all_in_one_train, all_in_one_test
 from xy_mse_loss import XYMSELoss
 
 Task = PushTask
+RobustTask = RobustPushTask
 modalities = ['gripper_sensors']
 
 # Parse args
@@ -32,13 +36,17 @@ parser = argparse.ArgumentParser()
 Task.add_dataset_arguments(parser)
 args = parser.parse_args()
 dataset_args = Task.get_dataset_args(args)
+parser = argparse.ArgumentParser()
+RobustTask.add_dataset_arguments(parser)
+args = parser.parse_args()
+robust_dataset_args = Task.get_dataset_args(args)
 
 fannypack.data.set_cache_path('datasets/gentle_push/cache')
 
 # Load trajectories into memory
 train_trajectories = Task.get_train_trajectories(**dataset_args)
 val_trajectories = Task.get_eval_trajectories(**dataset_args)
-test_trajectories = Task.get_test_trajectories(**dataset_args)
+haptics_robust_trajectories = RobustTask.get_test_trajectories(haptics_noise=True, **robust_dataset_args)
 
 train_loader = DataLoader(
     SubsequenceDataset(train_trajectories, 16, modalities),
@@ -51,30 +59,32 @@ val_loader = DataLoader(
     batch_size=32,
     shuffle=True,
 )
-test_loader = DataLoader(
-    SubsequenceDataset(test_trajectories, 16, modalities),
-    batch_size=32,
-    shuffle=False,
-)
+haptics_robust_loader = []
+for i in range(len(haptics_robust_trajectories)):
+    haptics_robust_loader.append(DataLoader(
+            RobustSubsequenceDataset(haptics_robust_trajectories[i], 16, modalities),
+            batch_size=32,
+            shuffle=False,
+        ))
+test_loaders = {
+    'haptics': haptics_robust_loader,
+}
 
 encoders = [
     Sequential(Transpose(0, 1), layers.observation_sensors_layers(64)),
 ]
 fusion = ConcatWithLinear(64, 64, concat_dim=2)
 head = Sequential(Head(), Transpose(0, 1))
-allmodules = [*encoders, fusion, head]
 optimtype = optim.Adam
 loss_state = nn.MSELoss()
 
-def trainprocess():
-    train(encoders, fusion, head,
-          train_loader, val_loader,
-          20,
-          task='regression',
-          optimtype=optimtype,
-          objective=loss_state,
-          lr=0.00001)
-all_in_one_train(trainprocess, allmodules)
+train(encoders, fusion, head,
+      train_loader, val_loader,
+      20,
+      task='regression',
+      optimtype=optimtype,
+      objective=loss_state,
+      lr=0.00001)
 
 model = torch.load('best.pt').cuda()
-test(model, test_loader, dataset='gentle push', task='regression', criterion=XYMSELoss())
+test(model, test_loaders, dataset='gentle push', task='regression', criterion=XYMSELoss())
