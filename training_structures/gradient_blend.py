1b34235086ed1a8947878d6ee33b2216fd01e1a3
kapikantzari <yuncheng@andrew.cmu.edu>
2021-06-15 11:37:19 -0400

    update with maxmin normalization

--- training_structures/gradient_blend.py
+++ training_structures/gradient_blend.py
@@ -362,7 +362,7 @@ def test(model, test_dataloaders_all, dataset, method_name='My method', auprc=Fa
         curve.append(v)
         robustness_curve[k] = curve 
     for measure, robustness_result in robustness_curve.items():
-      print("relative robustness ({}, {}): {}".format(noisy_modality, measure, str(relative_robustness(robustness_result))))
+      print("relative robustness ({}, {}): {}".format(noisy_modality, measure, str(relative_robustness(robustness_result, robustness_key))))
       robustness_key = dataset
       robustness_key = '{} {}'.format(dataset, noisy_modality)
       if len(robustness_curve) != 1:
