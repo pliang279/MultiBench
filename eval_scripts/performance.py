b2ebed862904ddcd3697b310bbbe2e332cbfb1be
Wu Zetian <ztwu_nil@zju.edu.cn>
2021-06-14 22:23:49 -0700

    refactored late_fusion_related mmimdb examples

--- eval_scripts/performance.py
+++ eval_scripts/performance.py
@@ -10,7 +10,7 @@ def AUPRC(pts):
   return sklearn.metrics.average_precision_score(true_labels, predicted_probs)
 
 def f1_score(truth, pred, average):
-    return sklearn.metrics.f1_score(truth.cpu().numpy(),pred.cpu().numpy(),average)
+    return sklearn.metrics.f1_score(truth.cpu().numpy(),pred.cpu().numpy(),average=average)
 
 def accuracy(truth, pred):
     return sklearn.metrics.accuracy_score(truth.cpu().numpy(),pred.cpu().numpy())
--- examples/multimedia/mmimdb_low_rank_tensor.py
+++ examples/multimedia/mmimdb_low_rank_tensor.py
@@ -1,24 +1,26 @@
 import sys
 import os
 sys.path.append(os.getcwd())
-from training_structures.Simple_Late_Fusion import train, test
+
+import torch
+
+from training_structures.Supervised_Learning import train, test
 from fusions.common_fusions import LowRankTensorFusion
 from datasets.imdb.get_data import get_dataloader
 from unimodals.common_models import MaxOut_MLP, Linear
-from torch import nn
-import torch
-filename = 'lowrank.pt'
-traindata, validdata, testdata = get_dataloader('../video/multimodal_imdb.hdf5', '/home/pliang/multibench/video/mmimdb', vgg=True, batch_size=128)
+
+filename = "best_lrtf.pt"
+traindata, validdata, testdata = get_dataloader('../video/multimodal_imdb.hdf5', '../video/mmimdb', vgg=True, batch_size=128)
 encoders=[MaxOut_MLP(512, 512, 300, linear_layer=False), MaxOut_MLP(512, 1024, 4096, 512, False)]
 head= Linear(512, 23).cuda()
 
 fusion=LowRankTensorFusion([512,512],512,128).cuda()
 
-train(encoders,fusion,head,traindata,validdata,1000, early_stop=True,task="multilabel", regularization=False,\
-    save="best_lrtf.pt", optimtype=torch.optim.AdamW,lr=8e-3,weight_decay=0.01, criterion=torch.nn.BCEWithLogitsLoss())
+train(encoders,fusion,head,traindata,validdata,1000, early_stop=True,task="multilabel",\
+    save=filename, optimtype=torch.optim.AdamW,lr=8e-3,weight_decay=0.01, objective=torch.nn.BCEWithLogitsLoss())
 
 print("Testing:")
-model=torch.load('best_lrtf.pt').cuda()
-test(model,testdata,dataset='imdb',criterion=torch.nn.BCEWithLogitsLoss(),task="multilabel")
+model=torch.load(filename).cuda()
+test(model,testdata,dataset='mmimdb',criterion=torch.nn.BCEWithLogitsLoss(),task="multilabel")
 
 
