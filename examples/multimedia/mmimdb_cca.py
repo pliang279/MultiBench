a3315024a18cea148903acf8d44a30477e48da4d
Wu Zetian <ztwu_nil@zju.edu.cn>
2021-06-14 22:59:11 -0700

    reorganize more codes for mmimdb

---
+++ examples/multimedia/mmimdb_cca.py
@@ -0,0 +1,26 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+
+import torch
+
+from training_structures.Supervised_Learning import train, test
+from objective_functions.objectives_for_supervised_learning import CCA_objective
+from fusions.common_fusions import Concat
+from datasets.imdb.get_data import get_dataloader
+from unimodals.common_models import Linear, MaxOut_MLP
+
+filename = "best_cca.pt"
+traindata, validdata, testdata = get_dataloader('../video/multimodal_imdb.hdf5', '../video/mmimdb', vgg=True, batch_size=800)
+
+outdim = 256
+encoders=[MaxOut_MLP(512, 512, 300, outdim, False), MaxOut_MLP(512, 1024, 4096, outdim, False)]
+head=Linear(2*outdim, 23).cuda()
+fusion=Concat().cuda()
+
+train(encoders,fusion,head,traindata,validdata,1000, early_stop=True,task="multilabel", save=filename,\
+     optimtype=torch.optim.AdamW,lr=1e-2,weight_decay=0.01, objective=CCA_objective(outdim, , torch.nn.BCEWithLogitsLoss()))
+
+print("Testing:")
+model=torch.load(filename).cuda()
+test(model,testdata,dataset='mmimdb',criterion=torch.nn.BCEWithLogitsLoss(),task="multilabel")
