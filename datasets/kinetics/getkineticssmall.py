68ca13022f6dfabe3a71b2869b7d80c9f4344fba
Peter Wu <peter.yh.wu@gmail.com>
2021-06-16 14:35:27 -0700

    added kinetics data preprocessing scripts

---
+++ datasets/kinetics/getkineticssmall.py
@@ -0,0 +1,69 @@
+import os
+import torch
+
+from torchvision.datasets import Kinetics400
+
+
+def getkinetics(datafolder,tempfolder,categorylist,frames_per_instance,reallabel,frame_skip=1,centercrop=None):
+    # TODO
+    # for category in categorylist:
+    #     os.system("mv "+datafolder+"/"+category+" "+tempfolder)
+    a = Kinetics400(tempfolder,300,extensions=('mp4',))
+    datas=[]
+    print("Total videos: "+str(len(a)))
+    for ii in range(len(a)):
+        (video,audio,label)=a[ii]
+        vh=len(video[0])
+        vw=len(video[0][0])
+        v=video.view(-1,frame_skip,vh,vw,3)[:,0,:,:,:].squeeze()
+        if centercrop is not None:
+            w,h=centercrop
+            if(w>vw)or(h>vh):
+                continue
+            hstart=(vh-h)//2
+            hend=hstart+h
+            wstart=(vw-w)//2
+            wend=wstart+w
+            v=v[:,hstart:hend,wstart:wend,:]
+        alen=len(audio[0]) # TODO this is wrong, should be 1
+        print(len(v))
+        '''
+        ap=alen*frames_per_instance*frame_skip//300
+        for i in range(len(v)//frames_per_instance):
+            vi=v[i*frames_per_instance:(i+1)*frames_per_instance]
+            alen=len(audio[0])
+            ai=audio[ap*i:ap*(i+1),:]
+            print(vi.shape)
+            print(ai.shape)
+            datas.append((vi,ai,reallabel))
+        '''
+    exit()
+
+    for category in categorylist:
+        os.system("mv "+tempfolder+"/"+category+" "+datafolder)
+    return datas
+
+
+def getdata(datalist,splitsize=50):
+    catacount = 0
+    trainhome = '/home/pliang/yiwei/kinetics/ActivityNet/Crawler/Kinetics/test_data/'
+    zemp_dir = trainhome+'zemp/'
+    if not os.path.exists(zemp_dir):
+        os.makedirs(zemp_dir)
+    for category in datalist:
+        files = os.listdir(trainhome+category)
+        for i in range((len(files)-1)//splitsize+1):
+            for j in range(0,splitsize):
+                if i*splitsize+j >= len(files):
+                    break
+                os.system('cp -r '+trainhome+category+'/'+files[i*splitsize+j]+' '+zemp_dir)
+                # os.system('mv '+trainhome+category+'/'+files[i*splitsize+j]+' '+trainhome+'zemp/')
+            # a=getkinetics(trainhome,trainhome,['zemp'],150,catacount,2,(224,224))
+            a=getkinetics(trainhome,'/home/pliang/yiwei/kinetics/ActivityNet/Crawler/Kinetics/temp',['zemp'],150,catacount,2,(224,224))
+            exit()
+            torch.save(a,'/data/yiwei/kinetics_small/test/'+category+str(i)+'.pt')
+            os.system('mv '+trainhome+'zemp/* '+trainhome+category)
+        catacount += 1
+
+getdata(['archery','breakdancing','crying','dining','singing'])
+
---
+++ datasets/kinetics/tobatch_37.py
@@ -0,0 +1,50 @@
+import torch
+import torchaudio
+import torchvision
+import os
+from tqdm import tqdm
+
+
+p = '/home/pliang/yiwei/kinetics/ActivityNet/Crawler/Kinetics/test_data/archery/002VmnaNvh4_000003_000013.mp4'
+sr = 44100
+audio_transform = torchaudio.transforms.MelSpectrogram(sample_rate=sr, n_mels=40)
+phase = 'train' # train, valid, test
+
+
+avgpool = torch.nn.AvgPool2d((2,2))
+datas = []
+batchcount=0
+for name in tqdm(os.listdir('%s' % phase)):
+    if name[-3:]=='.pt':
+        # print('at '+name)
+        f = torch.load('%s/%s' % (phase, name))
+        for tensors in f:
+            a = tensors[1] # 1, 152576, values btw -1 and 1
+            if len(a) != 0:
+                if len(a) == 1:
+                    a = a[0]
+                elif len(a) == 2:
+                    a = a.mean(0)
+                a -= a.min()
+                if a.min() != a.max():
+                    a /= a.max()
+                    a = 2*a - 1 # values in [-1, 1]
+                spec = audio_transform(a)
+                spec = spec + 1e-10
+                spec = spec.log()
+
+                v = tensors[0]
+                l = tensors[2]
+                v = ((v.float()/255.0)-torch.FloatTensor([0.43216, 0.394666, 0.37645]))/torch.FloatTensor([0.22803, 0.22145, 0.216989])
+                v = v.transpose(0,3).transpose(3,1)
+                v = avgpool(v)
+                
+                datas.append((v,spec,l))
+
+                if len(datas)==100:
+                    torch.save(datas,'%s/batch_37%s.pdt' % (phase, batchcount))
+                    batchcount+=1
+                    datas=[]
+
+torch.save(datas,'%s/batch_37%s.pdt' % (phase, batchcount))
+
