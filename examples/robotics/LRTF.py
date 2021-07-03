2a3ddb0b94b9ae8daa1a353491c31d0e3b2dad24
lvyiwei1 <ylyu1@andrew.cmu.edu>
2021-07-02 22:56:59 -0400

    fix

--- examples/robotics/LRTF.py
+++ examples/robotics/LRTF.py
@@ -64,7 +64,7 @@ class selfsupervised:
         # losses
         self.loss_contact_next = nn.BCEWithLogitsLoss()
 
-        self.train_loader, self.val_loader = get_data(self.device, self.configs,"/home/pliang/multibench/MultiBench-robotics/")
+        self.train_loader, self.val_loader, _ = get_data(self.device, self.configs,"/home/pliang/multibench/MultiBench-robotics/")
 
     def train(self):
         print(len(self.train_loader.dataset), len(self.val_loader.dataset))
