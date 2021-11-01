78ffe731e32096eba2ef5b9e9bc6b9bf25e4eaf2
vanvan2017 <1134559300@qq.com>
2021-11-01 12:00:42 +0800

    updated data and new feature of affect dataloaders in ReadMe

--- examples/affect/affect_early_fusion.py
+++ examples/affect/affect_early_fusion.py
@@ -12,21 +12,21 @@ from training_structures.Supervised_Learning import train, test
 
 
 # mosi_raw.pkl, mosei_raw.pkl, sarcasm.pkl, humor.pkl
-# traindata, validdata, testdata = get_dataloader('/home/pliang/multibench/affect/pack/mosi/mosi_raw.pkl', robust_test=False)
-traindata, validdata, testdata = get_dataloader('/home/pliang/multibench/affect/sarcasm.pkl', robust_test=False, max_pad=True, data_type='sarcasm', max_seq_len=50)
+traindata, validdata, testdata = get_dataloader('/home/paul/MultiBench/mosi_raw.pkl')
 
 # mosi/mosei
-# encoders = [Identity().cuda(),Identity().cuda(),Identity().cuda()]
-# head = Sequential(GRU(409,512,dropout=True,has_padding=True),MLP(512, 512, 1)).cuda()
+encoders = [Identity().cuda(),Identity().cuda(),Identity().cuda()]
+head = Sequential(GRU(409,512,dropout=True,has_padding=True),MLP(512, 256, 1)).cuda()
 
 # humor/sarcasm
-encoders = [Identity().cuda(),Identity().cuda(),Identity().cuda()]
-head = Sequential(GRU(752, 1128, dropout=True, has_padding=False, batch_first=True), MLP(1128, 512, 1)).cuda()
+# encoders = GRU(752, 1128, dropout=True, has_padding=True).cuda()
+# head = MLP(1128, 512, 1).cuda()
 
 fusion = ConcatEarly().cuda()
 
-train(encoders, fusion, head, traindata, testdata, 100, task="regression", optimtype=torch.optim.AdamW,is_packed=False, lr=1e-3, save='sarcasm_ef_r0.pt', weight_decay=0.01, objective=torch.nn.L1Loss())
+train(encoders, fusion, head, traindata, validdata, 1000, task="regression", optimtype=torch.optim.AdamW,is_packed=True,
+        lr=1e-4, save='mosi_ef_best.pt', weight_decay=0.01, objective=torch.nn.L1Loss())
 
 print("Testing:")
-model = torch.load('sarcasm_ef_r0.pt').cuda()
-test(model, testdata,'affect', is_packed=False, criterion=torch.nn.L1Loss(), task="posneg-classification", no_robust=True)
+model = torch.load('mosi_ef_best.pt').cuda()
+test(model, testdata,'affect', is_packed=True, criterion=torch.nn.L1Loss(), task="posneg-classification")
