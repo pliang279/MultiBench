a7c7d822253b3300d407c9cf80dcafeef87bcc46
Arav Agarwal <aravagar@umich.edu>
2022-03-28 02:40:28 -0400

    Add test-fusion

--- tests/test_fusion.py
+++ tests/test_fusion.py
@@ -5,9 +5,9 @@ def test_common():
     fusion = Concat()
     assert fusion([torch.zeros((1,2)) for _ in range(2)]).shape == (1,4)
     fusion = ConcatEarly()
-    assert fusion([torch.zeros((1,2,2)) for _ in range(2)]).shape == (1,2,4)
-    
+    assert fusion([torch.zeros((1,2,2)) for _ in range(2)]).shape == (1,2,4) 
     fusion = Stack()
     assert fusion([torch.zeros((1,2,2)) for _ in range(2)]).shape == (1,2,4)
     
-    
+    fusion = ConcatWithLinear(2,2)
+    assert fusion([torch.zeros((1,2,2)) for _ in range(2)]).shape == (1,2,4)
