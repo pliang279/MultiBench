0c3b01b7bc7ed81e5772ceaf4cec4fa23249e210
Arav Agarwal <aravagar@umich.edu>
2022-03-28 02:39:54 -0400

    Add test-fusion

---
+++ tests/test_fusion.py
@@ -0,0 +1,13 @@
+from fusions.common_fusions import *
+
+def test_common():
+    """."""
+    fusion = Concat()
+    assert fusion([torch.zeros((1,2)) for _ in range(2)]).shape == (1,4)
+    fusion = ConcatEarly()
+    assert fusion([torch.zeros((1,2,2)) for _ in range(2)]).shape == (1,2,4)
+    
+    fusion = Stack()
+    assert fusion([torch.zeros((1,2,2)) for _ in range(2)]).shape == (1,2,4)
+    
+    
