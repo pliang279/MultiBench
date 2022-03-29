7d2569ed19b89d5dc9bc91d45f88313a9de928d3
Arav Agarwal <aravagar@umich.edu>
2022-03-28 22:01:37 -0400

    Add test-fusion

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,14 +1,15 @@
 import torch
 import numpy as np
-def test_blah():
+
+    
+def test_sl():
     from unimodals.common_models import GRU, MLP, Sequential, Identity  # noqa
     from training_structures.Supervised_Learning import train, test  # noqa
-    from datasets.affect.get_data import get_dataloader  # noqa
     from fusions.common_fusions import ConcatEarly  # noqa
-
     data = [torch.zeros([32, 50, 35]), torch.zeros([32, 50, 74]), torch.zeros([32, 50, 300]), torch.cat((torch.ones((16,1)),torch.zeros((16,1))),dim=0)]
     my_dataset = torch.utils.data.TensorDataset(*data)
     dl = torch.utils.data.DataLoader(my_dataset)
+
     encoders = [Identity().to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu")), Identity().to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu")), Identity().to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))]
     head = Sequential(GRU(409, 512, dropout=True, has_padding=False,
                   batch_first=True, last_only=True), MLP(512, 512, 1)).to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
